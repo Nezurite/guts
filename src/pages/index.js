1f56291dc1ec0cf23e5a81be44c1f9e244f071cf
Nezurite <enfinrecup444@gmail.com>
2025-06-01 01:15:16 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Chat from '../components/Chat';
+import { useChatStore } from '../lib/chatStore';
 
 export default function Home() {
+  const { getChats } = useChatStore();
+  const [activeChat, setActiveChat] = useState(() => {
+    const chats = getChats();
+    return chats.length > 0 ? chats[0].id : null;
+  });
+
   return (
     <div className="app flex">
-      <Sidebar />
-      <Chat />
+      <Sidebar activeChat={activeChat} setActiveChat={setActiveChat} />
+      {activeChat ? (
+        <Chat activeChat={activeChat} />
+      ) : (
+        <div className="empty-chat">Select or create a chat</div>
+      )}
     </div>
   );
 }
